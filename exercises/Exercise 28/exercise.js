4ff4f64cf8b6444a02f027e3f6ab18b9d3e12038
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2022-02-20 18:51:08 +0000

    Added spheres

--- exercises/Exercise 28/exercise.js
+++ exercises/Exercise 28/exercise.js
@@ -8,24 +8,24 @@ d3.select('body')
 
 // Load data from CSV
 d3.csv("https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalyticsLab2/master/data/spheres.csv").then(function(data) {
-        
+    
     // Create simulation
     const simulation = d3.forceSimulation(data)
         .force('charge', d3.forceManyBody().strength(5))
         .force('center', d3.forceCenter(width/2, height/2))
         .force('collision', d3.forceCollide().radius( (d) => d.radius ))
         .on('tick', tick);
+    
+    // Update graphics each frame
+    function tick() {
+        const u = d3.select('svg')
+            .selectAll('circle')
+            .data(data)
+            .join('circle')
+            .attr('fill', (d) => d.color)
+            .attr('stroke', "black")
+            .attr('r', (d) => d.radius)
+            .attr('cx', (d) => d.x)
+            .attr('cy', (d) => d.y)
+    }
 });
-
-// Update graphics each frame
-function tick() {
-    const u = d3.select('svg')
-        .selectAll('circle')
-        .data(data)
-        .join('circle')
-        .attr('fill', (d) => d.color)
-        .attr('stroke', "black")
-        .attr('r', (d) => d.radius)
-        .attr('cx', (d) => d.x)
-        .attr('cy', (d) => d.y)
-}
